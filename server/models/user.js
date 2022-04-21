56804c04fde1b84c37948b3b346d34676a198fc2
royalbluee <tkyeom96@gmail.com>
2022-04-21 17:24:20 +0900

    feat(이태겸): Modify userSchema

--- server/models/user.js
+++ server/models/user.js
@@ -4,9 +4,12 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema(
   {
     id: { type: Number, required: true, unique: true },
-    address: { type: String, required: true },
+    email: { type: String, required: true },
+    pw: { type: String, required: true },
     name: { type: String, required: true },
     desc: { type: String },
+    // 니모닉 지갑 관련된 정보들도 추가
+    address: { type: String, required: true },
     // post_created 시간은 계정 만드는 시간이랑 다름 Check 필요
     // post_created_at: { type: timestamp },
   },
