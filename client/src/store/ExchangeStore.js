a272917b1b7421968fde8d87f8304df2dbc978b4
royalbluee <tkyeom96@gmail.com>
2022-04-26 16:53:39 +0900

    feat(이태겸): Modify handler in SignIn and SignUp

--- client/src/store/ExchangeStore.js
+++ client/src/store/ExchangeStore.js
@@ -2,15 +2,35 @@ import { makeAutoObservable } from 'mobx';
 
 class ExchangeStore {
   // 선언(초기값)
-  email = 'text';
+  accessToken = '';
+  userID = 0;
+  email = '';
+  desc = '';
+  address = '';
 
   constructor() {
     makeAutoObservable(this);
   }
 
   // 설정
+  setAccessToken(accessToken) {
+    this.accessToken = accessToken;
+  }
+
+  setUserID(userID) {
+    this.userID = userID;
+  }
+
   setEmail(email) {
     this.email = email;
+  }
+
+  setDesc(desc) {
+    this.desc = desc;
+  }
+
+  setAddress(address) {
+    this.address = address;
   }
 }
 
