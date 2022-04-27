3cb8c45b5e49d47b4f0981c1e852dd63996b41b3
royalbluee <tkyeom96@gmail.com>
2022-04-27 17:58:59 +0900

    feat(이태겸): Add erc20 contract

--- server/models/user.js
+++ server/models/user.js
@@ -10,6 +10,7 @@ const userSchema = new mongoose.Schema(
     desc: { type: String },
     address: { type: String, required: true },
     privateKey: { type: String, required: true },
+    token: { type: Number, default: 10 },
     // post_created 시간은 계정 만드는 시간이랑 다름 Check 필요
     // post_created_at: { type: timestamp },
   },
