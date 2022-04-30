bf451341d9e48c54c34b7d4715d0117c52676366
royalbluee <tkyeom96@gmail.com>
2022-05-01 00:43:45 +0900

    feat(이태겸): Add recommentList

--- client/src/components/RecommendList.jsx
+++ client/src/components/RecommendList.jsx
@@ -1,23 +1,45 @@
-import React from "react";
-import { Container, Grid, Box, Typography } from "@mui/material";
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Container, Grid, Box, Typography } from '@mui/material';
 
 export default function RecomList() {
-  const myInfo = JSON.parse(localStorage.getItem("userInfo"));
+  const [recommand, setRecommand] = useState('');
+  const myInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+  useEffect(() => {
+    const getRecomList = async () => {
+      const { data } = await axios.post(
+        'http://localhost:4000/recomList',
+        {
+          address: myInfo.address,
+        },
+        {
+          headers: {
+            authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+          },
+        }
+      );
+      setRecommand(data.data.recomList);
+    };
+    getRecomList();
+  }, []);
+
+  console.log(recommand);
+
   return (
     <>
-      <Container sx={{ width: "700px" }}>
-        <Box sx={{ position: "fixed", width: "390px" }}>
-          <Grid item sx={{ borderBottom: "1px solid #aaa" }}>
-            <Box display={"flex"} p={3}>
+      <Container sx={{ width: '700px' }}>
+        <Box sx={{ position: 'fixed', width: '390px' }}>
+          <Grid item sx={{ borderBottom: '1px solid #aaa' }}>
+            <Box display={'flex'} p={3}>
               <Box
                 mt={3}
                 sx={{
                   width: 100,
                   height: 100,
                   borderRadius: 100,
-                  background:
-                    'url("https://source.unsplash.com/collection/1") center center',
-                  backgroundSize: "cover",
+                  background: `url("https://source.unsplash.com/collection/${myInfo.id}") center center`,
+                  backgroundSize: 'cover',
                 }}
               ></Box>
               <Box m={2}>
@@ -28,47 +50,35 @@ export default function RecomList() {
           </Grid>
           <Grid item mt={4}>
             <Typography
-              component="p"
-              variant="p"
+              component='p'
+              variant='p'
               pl={2}
-              sx={{ fontFamily: "Jua", color: "#aaa" }}
+              sx={{ fontFamily: 'Jua', color: '#aaa' }}
             >
               회원님을 위한 추천
             </Typography>
-            <Box display={"flex"} pt={2} pl={2}>
-              <Box
-                mt={2}
-                sx={{
-                  width: 65,
-                  height: 65,
-                  borderRadius: 100,
-                  background:
-                    'url("https://source.unsplash.com/collection/2") center center',
-                  backgroundSize: "cover",
-                }}
-              ></Box>
-              <Box m={1} sx={{ fontSize: "0.75rem" }}>
-                <h2>닉네임</h2>
-                <p>설명글</p>
-              </Box>
-            </Box>
-            <Box display={"flex"} pl={2}>
-              <Box
-                mt={2}
-                sx={{
-                  width: 65,
-                  height: 65,
-                  borderRadius: 100,
-                  background:
-                    'url("https://source.unsplash.com/collection/3") center center',
-                  backgroundSize: "cover",
-                }}
-              ></Box>
-              <Box m={1} sx={{ fontSize: "0.75rem" }}>
-                <h2>닉네임</h2>
-                <p>설명글</p>
-              </Box>
-            </Box>
+            {!!recommand && (
+              <>
+                {recommand.map((el) => (
+                  <Box display={'flex'} pt={2} pl={2}>
+                    <Box
+                      mt={2}
+                      sx={{
+                        width: 65,
+                        height: 65,
+                        borderRadius: 100,
+                        background: `url("https://source.unsplash.com/collection/${el.id}") center center`,
+                        backgroundSize: 'cover',
+                      }}
+                    ></Box>
+                    <Box m={1} sx={{ fontSize: '0.75rem' }}>
+                      <h2>{el.name}</h2>
+                      <p>{el.desc}</p>
+                    </Box>
+                  </Box>
+                ))}
+              </>
+            )}
           </Grid>
         </Box>
       </Container>
