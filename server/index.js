199aded92e4f1b879abeca54581b0be7f509fa21
BCSungjin <asdf256617@gmail.com>
2022-05-01 23:17:33 +0900

    npm i path

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
+const path = require('path')
 
 const mongoose = require('mongoose');
 
